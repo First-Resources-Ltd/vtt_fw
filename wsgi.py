49b8558b0e96bdbfc122967e1f8f687c71f8d81d
Marvin Priyatno <marvin.priyatno@first-resources.co.id>
2023-07-12 15:41:43 +0700

    debug wsgi true

--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,5 @@ import os
 from app import server
 
 if __name__ == "__main__":
-    server.run(host='0.0.0.0', port=os.environ['EXPOSE_PORT'], debug=os.environ['DEBUG'])
+    server.run(host='0.0.0.0', port=os.environ['EXPOSE_PORT'], debug=True)
 
