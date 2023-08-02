c5a5998b88c2529a5ced190da14adada3ce67411
Marvin Priyatno <marvin.priyatno@first-resources.co.id>
2023-08-02 16:53:40 +0700

    simplify code

--- app.py
+++ app.py
@@ -25,48 +25,50 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in os.environ['ALLOWED_EXTENSIONS']
 
-@server.route('/test_mp3', methods=['GET'])
-def test_mp3():
-    tempFile = "audiobook.mp3"
-    # 05 Set up a process manager
+def process_transcribe(tempFile: str):
+    # 01 Set up a process manager
     process_manager = Manager()
     shared_data = process_manager.dict()
     
-    # 06 Create child process for whisper execution
+    # 02 Create child process for whisper execution
     p = Process(target=_do_transcribe, args=(shared_data, "result", tempFile))
     
     p.start()      # run child process
     p.join()       # wait for the process to complete
     p.terminate()  # terminate the process (optional - should happen automatically)
 
-    # 07 Remove uploaded file
-    if os.path.isfile(tempFile):
-        os.remove(tempFile)
+    # 03 Get transcription from shared data and delete
+    return shared_data["result"]
 
-    # 08 Get transcription from shared data and delete
-    data = shared_data["result"]
+def _do_transcribe(shared_data: dict, rid: str, tempFile: str):
+    try:
+        model = WhisperModel(modelPath, compute_type="int8")
+        segments, info = model.transcribe(tempFile, beam_size=1)
+        data = ''
+        for segment in segments:
+            data += segment.text
+
+        # Store data in shared data
+        shared_data[rid] = data
+    except Exception as e:
+        # Handle the error, if needed
+        print(f"Error: {e}")
+        shared_data[rid] = None  # Store None in the shared data to indicate an error
+    finally:
+        # Release model resources
+        del model
+        del info
+
+@server.route('/test_mp3', methods=['GET'])
+def test_mp3():
+    tempFile = "audiobook.mp3"
+    data = process_transcribe(tempFile)
     return {'success': True, 'data':data}
 
 @server.route('/test_wav', methods=['GET'])
 def test_wav():
     tempFile = "indonesian.wav"
-    # 05 Set up a process manager
-    process_manager = Manager()
-    shared_data = process_manager.dict()
-    
-    # 06 Create child process for whisper execution
-    p = Process(target=_do_transcribe, args=(shared_data, "result", tempFile))
-    
-    p.start()      # run child process
-    p.join()       # wait for the process to complete
-    p.terminate()  # terminate the process (optional - should happen automatically)
-
-    # 07 Remove uploaded file
-    if os.path.isfile(tempFile):
-        os.remove(tempFile)
-
-    # 08 Get transcription from shared data and delete
-    data = shared_data["result"]
+    data = process_transcribe(tempFile)
     return {'success': True, 'data':data}
 
 @server.route('/transcript', methods=['POST'])
@@ -98,33 +100,9 @@ def transcript():
     tempFile = os.path.join(os.environ['UPLOAD_FOLDER'], f'{tempName}_{audio_file.filename}')
     
     # 04 Read Open & Write audio data to file uploaded files
-    # audio_file.save(tempFile)
-    audio_data = audio_file.read()
-    with open(tempFile, 'wb') as f:
-        f.write(audio_data)
-        f.close()
-    # 05 Set up a process manager
-    process_manager = Manager()
-    shared_data = process_manager.dict()
+    audio_file.save(tempFile)
     
-    # 06 Create child process for whisper execution
-    p = Process(target=_do_transcribe, args=(shared_data, "result", tempFile))
-    
-    p.start()      # run child process
-    p.join()       # wait for the process to complete
-    p.terminate()  # terminate the process (optional - should happen automatically)
-
-    # 07 Remove uploaded file
-    if os.path.isfile(tempFile):
-        os.remove(tempFile)
-
-    # 08 Get transcription from shared data and delete
-    data = shared_data["result"]
-    del audio_file
-    del audio_data
-    del tempFile
-    del shared_data
-    del process_manager
+    data = process_transcribe(tempFile)
 
     return {'success': True, 'data':data}
 
@@ -140,80 +118,3 @@ def clear_uploaded_files():
     removedStr = str(lenFile)
     return {'message' : 'Removed ' + removedStr + ' Files'}
 
-@server.route('/transcript_new', methods=['POST'])
-def transcript_new():
-
-    # 01 field, files Validation
-    audio_file = request.files.get("audio_file")
-    if not audio_file :
-        return {
-            'data': False,
-            'success': False,
-            'message': 'Failed transcript audio to text, Request field audio_file is requied and must be audio file .mp3 or .wav',
-        }
-
-    # 02 format validation 
-    allowed = allowed_file(audio_file.filename)
-    if not allowed :
-        return {
-            'data': False,
-            'success': False,
-            'message': f'Failed transcript audio to text, File Name {audio_file.filename} Not Allowed',
-        }
-
-    if not os.path.exists(os.environ['UPLOAD_FOLDER']):
-        os.makedirs(os.environ['UPLOAD_FOLDER'])
-
-    # 03 Write unique file
-    tempName = str(uuid.uuid4())
-    tempFile = os.path.join(os.environ['UPLOAD_FOLDER'], f'{tempName}_{audio_file.filename}')
-    
-    # 04 Read Open & Write audio data to file uploaded files
-    # audio_file.save(tempFile)
-    audio_data = audio_file.read()
-    with open(tempFile, 'wb') as f:
-        f.write(audio_data)
-        f.close()
-    # 05 Set up a process manager
-    process_manager = Manager()
-    shared_data = process_manager.dict()
-    
-    # 06 Create child process for whisper execution
-    p = Process(target=_do_transcribe, args=(shared_data, "result", tempFile))
-    
-    p.start()      # run child process
-    p.join()       # wait for the process to complete
-    p.terminate()  # terminate the process (optional - should happen automatically)
-
-    # 07 Remove uploaded file
-    if os.path.isfile(tempFile):
-        os.remove(tempFile)
-
-    # 08 Get transcription from shared data and delete
-    data = shared_data["result"]
-    del audio_file
-    del audio_data
-    del tempFile
-    del shared_data
-    del process_manager
-
-    return {'success': True, 'data':data}
-
-def _do_transcribe(shared_data: dict, rid: str, tempFile: str):
-    try:
-        model = WhisperModel(modelPath, compute_type="int8")
-        segments, info = model.transcribe(tempFile, beam_size=1)
-        data = ''
-        for segment in segments:
-            data += segment.text
-
-        # Store data in shared data
-        shared_data[rid] = data
-    except Exception as e:
-        # Handle the error, if needed
-        print(f"Error: {e}")
-        shared_data[rid] = None  # Store None in the shared data to indicate an error
-    finally:
-        # Release model resources
-        del model
-        del info
